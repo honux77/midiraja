47a60037642736528e53f01487aaa38cc2618796
Park, Sungchul <gyumee@gmail.com>
2025-08-28 10:08:38 +0900

    chore: add package-info.java files for strict null-safety annotations

---
+++ src/main/java/com/midiraja/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2026, Park, Sungchul
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+@NullMarked
+package com.midiraja;
+
+import org.jspecify.annotations.NullMarked;
