abb4a19be34df47796bd6d88466f3d33f9813c25
Park, Sungchul <gyumee@gmail.com>
2026-03-13 14:20:17 +0900

    chore: remove stale C build artifacts from src/main/c/, guard with .gitignore

--- src/main/c/adlmidi/version_check.c
+++
@@ -1,5 +0,0 @@
-
-    #include "adlmidi.h"
-    #if !(ADLMIDI_VERSION_MAJOR == 1 && ADLMIDI_VERSION_MINOR == 6 && ADLMIDI_VERSION_PATCHLEVEL == 2)
-    #error Project and source code version do not match!
-    #endif
--- src/main/c/munt/CMakeFiles/mt32emu.dir/src/LA32Ramp.cpp.o.d
+++
@@ -1,8 +0,0 @@
-CMakeFiles/mt32emu.dir/src/LA32Ramp.cpp.o: \
-  /Users/sungchulpark/Documents/projects/midiraja/ext/munt/mt32emu/src/LA32Ramp.cpp \
-  /Users/sungchulpark/Documents/projects/midiraja/ext/munt/mt32emu/src/internals.h \
-  /Users/sungchulpark/Documents/projects/midiraja/ext/munt/mt32emu/src/Types.h \
-  /Users/sungchulpark/Documents/projects/midiraja/ext/munt/mt32emu/src/LA32Ramp.h \
-  /Users/sungchulpark/Documents/projects/midiraja/ext/munt/mt32emu/src/globals.h \
-  /Users/sungchulpark/Documents/projects/midiraja/src/main/c/munt/include/mt32emu/config.h \
-  /Users/sungchulpark/Documents/projects/midiraja/ext/munt/mt32emu/src/Tables.h
